0189468d2cbf14a15c6cdefe75048fa72cdc1f80
tiagodpessoa <pessoatiago439@gmail.com>
2024-05-26 20:36:50 -0300

    Subindo projeto.

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,5 @@ import banco.ContaPoupanca;
 
 public class Main {
     public static void main(String[] args) {
-        Cliente tiago = new Cliente();
-        Cliente lucas = new Cliente();
-        tiago.setNome("Tiago");
-        lucas.setNome("Lucas");
-        Conta cc = new ContaCorrente(lucas);
-        Conta cp = new ContaPoupanca(tiago);
-
-        cc.depositar(100);
-        cc.transferir(cp, 50);
-
-        cc.imprimirExtrato();
-        cp.imprimirExtrato();
     }
 }
